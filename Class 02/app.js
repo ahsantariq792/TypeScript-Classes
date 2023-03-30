5973a6f185a58060559bc336f6113ba69bd78807
ahsantariq792 <tariqahsan63@gmail.com>
2023-03-31 04:27:17 +0500

    class 02

--- Class 02/app.js
+++ Class 02/app.js
@@ -17,3 +17,34 @@ customers.push({
     email: ""
 });
 console.log(customers);
+var roles = [0, "Admin"];
+// const roles2 : [number, string] = [0, "Admin","Admin"]
+//error
+// but work with push
+roles.push("adadasdaaaf");
+var data;
+data = "asahada";
+data = 2;
+////////////////////
+// Literal types ///
+///////////////////
+// function myFunct(num1: number, num2: number, cond: String) {
+function myFunct(num1, num2, cond) {
+    if (cond === "add") {
+        return num1 + num2;
+    }
+    else if (cond === "subtract") {
+        return num1 - num2;
+    }
+}
+// console.log(myFunct(2, 3, "divide"))
+//error the conditions are defined only they can be used
+///////////////////
+// Function Type///
+///////////////////
+var myFunc;
+//if we have to give return type of call back function in the parameter it can be given in the same way
+function add(a, b) {
+    return a + b;
+}
+myFunc = add;
--- Class 02/app.ts
+++ Class 02/app.ts
@@ -35,9 +35,69 @@ customers.push(person1);
 
 customers.push({
     name: "Nomankhanjan",
-    age:12,
-    hobbies: ["book reading","fishing"],
+    age: 12,
+    hobbies: ["book reading", "fishing"],
     email: ""
 });
 
 console.log(customers)
+
+
+
+
+/////////////
+/////tuples//
+/////////////
+
+//Fixed Length array when we know data types of its elemants and they are different
+  
+const roles: [number, string] = [0, "Admin"]
+
+// const roles2 : [number, string] = [0, "Admin","Admin"]
+//error
+
+// but work with push
+
+roles.push("adadasdaaaf")
+
+
+let data: string | number
+
+data = "asahada"
+data = 2
+
+
+////////////////////
+// Literal types ///
+///////////////////
+
+
+// function myFunct(num1: number, num2: number, cond: String) {
+function myFunct(num1: number, num2: number, cond: "add" | "subtract") {
+    if (cond === "add") {
+        return num1 + num2
+    }
+    else if (cond === "subtract") {
+        return num1 - num2
+    }
+}
+
+// console.log(myFunct(2, 3, "divide"))
+//error the conditions are defined only they can be used
+
+
+///////////////////
+// Function Type///
+///////////////////
+
+
+let myFunc: (a: number, b: number) => number;
+
+//if we have to give return type of call back function in the parameter it can be given in the same way
+
+function add(a: number, b: number,): number//(return data type of function) 
+{
+    return a + b;
+}
+
+myFunc = add;
